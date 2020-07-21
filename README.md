44ed71eded68ae1c213e8f426d8bd37fee58d661
Colon E Bracket <>
2020-07-21 01:35:47 -0500

    Rewrote the flip function to not print each coin

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ A discord bot to help you:
 * Roll dice!
 * Flip coins!
 
-### Other Neat Features!
+It's not the best bot for rolling dice or playing Dungeons and Dragons, but it's a fun bot to work on and bring to life!
+
+## Other Neat Features!
+
 * Custom status messages!
+
 ![What a creative status!](./screenshots/status.gif)
+
+## Special Thanks
+Thanks to Jack for relentlessly trying to break my bot and even causing crashes along the way.
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./bot_config.json');
-var counterActivity = 0;
+const int_flipLimit = Math.floor(2000/8); //Not the best calculation, just a guess at what the limit would be. Will handle the character limit issue better.
 
 // When the bot succesfully logs in
 client.on('ready', () => {
@@ -10,7 +10,7 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	setInterval(() => {
         setRandomActivity(activities);
-    }, 5*60*1000); // Runs this every 5 minutes.
+    }, 5*60*1000); // Changes activity every 5 minutes.
 });
 
 // When a message is sent in the server
@@ -32,28 +32,38 @@ client.on('message', msg => {
 client.login(config.token);
 
 function coinFlip(msg, times = 1){
-	times = parseInt(times);
-	if(times == NaN){
+	times = parseInt(times); // Sanitize the input! *scrubscrubscrub*
+	if(isNaN(times)){ // Can't flip a non-number amount...
 		msg.channel.send("I'm not sure how many coins that is... :thinking:");
-		console.log(typeof times);
 		return;
 	}
-	if(times > 1000){
-		msg.channel.send("My tiny hands can't flip more than 1000 coins :cry:");
+	// TO-DO - Use time measurement to set a cut-off time on calculating the outcome of a coin flip
+	if(times > 10**6){ // Too many coins will hang up the bot.
+		msg.channel.send("I'm too weak to flip more than a million coins :pensive:");
 		return;
 	}
+	// This code is not ideal but it works for now :pensive:
 	if(times == 1) outcome = "";
-	else outcome = `__You flipped ${times} coins__\n`;
+	else outcome = `__You flipped ${times} coins__\n`
 	
+	int_headsCount = 0;
+	int_tailsCount = 0;
 	do {
-		value = (Math.floor(Math.random() * 2) == 0);
-		if (value)
-			outcome += "**HEADS** ";
+		flipValue = (Math.floor(Math.random() * 2) == 0);
+		if (flipValue)
+			int_headsCount++;
 		else
-			outcome += "_tails_ ";
-		
+			int_tailsCount++;	
 		times--;
 	} while (times > 0);
+	if(outcome == ""){
+		if (int_headsCount)
+			outcome += "**HEADS**";
+		else
+			outcome += "_tails_";
+		return msg.channel.send(outcome);
+	}
+	outcome += `**HEADS**: ${int_headsCount}\n_tails_: ${(int_tailsCount)}`;
 	return msg.channel.send(outcome);
 }
 
